512ccaad8e738f174b9752ec629f0e980a846800
acaZdr <zdravkovic.aleksandar21@gmail.com>
2024-04-27 23:23:25 +0200

    Implemented parse_evaluation_response in llama_requests.py

--- api_server/server.py
+++ api_server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from llama_requests import LlamaResponseParser
 
 app = Flask(__name__)
 
@@ -8,10 +9,12 @@ data = [
     {'id': 2, 'name': 'Alice'}
 ]
 
+
 # Endpoint to get all data
 @app.route('/api/data', methods=['GET'])
 def get_data():
     return jsonify(data)
+
 
 # Endpoint to get data by ID
 @app.route('/api/data/<int:id>', methods=['GET'])
@@ -22,6 +25,7 @@ def get_data_by_id(id):
     else:
         return jsonify({'message': 'Data not found'}), 404
 
+
 # Endpoint to add new data
 @app.route('/api/data', methods=['POST'])
 def add_data():
@@ -29,6 +33,15 @@ def add_data():
     data.append(new_data)
     return jsonify({'message': 'Data added successfully'}), 201
 
+
+@app.route('/api/getEvaluation', methods=['POST'])
+def get_evaluation():
+    data = request.get_json()
+    response = LlamaResponseParser.post_to_llama(data['text'])
+    evaluation = LlamaResponseParser.parse_evaluation_response(response)
+    return jsonify(evaluation), 201
+
+
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=2342)
